cbf4abb8a5265cdb37741063c6b3c5b04746182f
AliceDavies27 <215499626+AliceDavies27@users.noreply.github.com>
2026-02-26 00:00:39 +0000

    CH3 L1: System Prompt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+from prompts import system_prompt
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
@@ -20,7 +21,7 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0))
 
     if response.usage_metadata == None:
         raise RuntimeError("API request failed.")
