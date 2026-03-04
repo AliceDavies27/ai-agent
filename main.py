eb96ad4855aeca4cbc18b785b45bbbe8f487e4af
AliceDavies27 <215499626+AliceDavies27@users.noreply.github.com>
2026-03-04 21:00:36 +0000

    CH4 L1: Agent Loop

--- main.py
+++ main.py
@@ -22,31 +22,44 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages,
+    final_response = None
+
+    for _ in range(20):
+
+        response = client.models.generate_content(model="gemini-2.5-flash", contents=messages,
                                               config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
 
-    if response.usage_metadata == None:
-        raise RuntimeError("API request failed.")
+        if response.usage_metadata == None:
+            raise RuntimeError("API request failed.")
     
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        
-    function_results = []
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    if len(response.function_calls) > 0:
-        for func_call in response.function_calls:
-            func_result = call_function(func_call, args.verbose)
-            if not len(func_result.parts) or not func_result.parts[0].function_response or not func_result.parts[0].function_response.response:
-                raise Exception("didn't get valid response from function call.")
-            function_results.append(func_result.parts[0])
-            if args.verbose:
-                print(f"-> {func_result.parts[0].function_response.response}")
-        
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
+        if response.function_calls:
+            function_results = []
+            for func_call in response.function_calls:
+                func_result = call_function(func_call, args.verbose)
+                if not len(func_result.parts) or not func_result.parts[0].function_response or not func_result.parts[0].function_response.response:
+                    raise Exception("didn't get valid response from function call.")
+                function_results.append(func_result.parts[0])
+                if args.verbose:
+                    print(f"-> {func_result.parts[0].function_response.response}")
+                messages.append(types.Content(role="user", parts=function_results))
+
+        else:
+            final_response = response.text
+            break
+    
+    if final_response is None:
+        print("Error: Maximum number of iterations reached")
+        exit(1)
     else:
-        print(response.text)
-
+        print(final_response)
 
 if __name__ == "__main__":
     main()
